0c9e43630a46771187c84195f79e2c4ea5d35568
M-Shourav <muhammadshourav625@gmail.com>
2025-07-05 11:00:00 +0600

    loading

--- src/components/blog/AllBlogPost.tsx
+++ src/components/blog/AllBlogPost.tsx
@@ -14,6 +14,7 @@ import { PostType } from "../../../types/postType";
 import axios from "axios";
 import { serverUrl } from "../../../utils/config";
 import Container from "../container";
+import { Skeleton } from "../ui/skeleton";
 
 const AllBlogPost = () => {
   const [postList, setPostList] = useState<PostType[]>([]);
@@ -37,48 +38,76 @@ const AllBlogPost = () => {
     getPostList();
   }, []);
   return (
-    <Container className="mt-6 grid grid-cols-1 lg:grid-cols-3 gap-5">
-      {postList?.map((post) => (
-        <Card key={post?.slug} className="group relative">
-          <CardHeader className="rounded-2xl">
-            <Image
-              src={post?.coverImage?.url}
-              alt="post-image"
-              width={800}
-              height={800}
-              className="aspect-[3/2] w-full rounded-2xl group-hover:scale-105 duration-500"
-              priority
-            />
-            <CardTitle className="mt-5">
-              <h3 className="text-sm/5 text-gray-500">
-                {dayjs(post?.createdAt).format("dddd,MMMM d,YYYY")}
-              </h3>
-              <Link
-                href={`/blog/${post?.slug}`}
-                className="text-base/7 font-semibold"
-              >
-                <span className=" absolute inset-0" />
-                {post?.title}
-              </Link>
-            </CardTitle>
-            <CardDescription>
-              <p className=" mt-2 text-base/5 text-gray-500 w-full">
-                {post?.summary}
-              </p>
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="flex items-center space-x-2 mt-auto">
-            <Image
-              src={post.author.coverImage?.url}
-              alt="author-image"
-              width={50}
-              height={50}
-              className="w-8 h-8 rounded-full"
-            />
-            <p className="text-sm/5 font-semibold">{post?.author?.name}</p>
-          </CardContent>
-        </Card>
-      ))}
+    <Container className="py-10">
+      {loading ? (
+        <div className="flex flex-wrap items-center justify-between gap-2">
+          <div className="flex items-center space-x-4 mt-5">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+          <div className="flex items-center space-x-4 mt-5 text-gray-700">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+          <div className="flex items-center space-x-4 mt-5">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="mt-6 grid grid-cols-1 lg:grid-cols-3 gap-5">
+          {postList?.map((post) => (
+            <Card key={post?.slug} className="group relative">
+              <CardHeader className="rounded-2xl">
+                <Image
+                  src={post?.coverImage?.url}
+                  alt="post-image"
+                  width={800}
+                  height={800}
+                  className="aspect-[3/2] w-full rounded-2xl group-hover:scale-105 duration-500"
+                  priority
+                />
+                <CardTitle className="mt-5">
+                  <h3 className="text-sm/5 text-gray-500">
+                    {dayjs(post?.createdAt).format("dddd,MMMM d,YYYY")}
+                  </h3>
+                  <Link
+                    href={`/blog/${post?.slug}`}
+                    className="text-base/7 font-semibold"
+                  >
+                    <span className=" absolute inset-0" />
+                    {post?.title}
+                  </Link>
+                </CardTitle>
+                <CardDescription>
+                  <p className=" mt-2 text-base/5 text-gray-500 w-full">
+                    {post?.summary}
+                  </p>
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="flex items-center space-x-2 mt-auto">
+                <Image
+                  src={post.author.coverImage?.url}
+                  alt="author-image"
+                  width={50}
+                  height={50}
+                  className="w-8 h-8 rounded-full"
+                />
+                <p className="text-sm/5 font-semibold">{post?.author?.name}</p>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      )}
     </Container>
   );
 };
